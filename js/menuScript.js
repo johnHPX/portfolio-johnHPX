19c54f5f3e1c6c591d224c784b2ebe976666a80a
jhonatasfreitas17 <jonatasfreitas008@gmail.com>
2022-09-03 22:23:19 -0300

    first commit.

---
+++ js/menuScript.js
@@ -0,0 +1,6 @@
+// var menuButton = document.getElementById("menuButton")
+var menu = document.getElementsByClassName("nav-main")[0]
+
+function openCloseMenu() {
+    menu.classList.toggle("active")
+}
